8821b980d9008e59f3bf189efc57f2db1e8e29ca
sameershaikh126878 <ywallah6@gmail.com>
2025-04-05 15:29:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @DOCTOR_ASP
+# Subscribe YouTube Channel For Amazing Bot https://t.me/TXT_UPDATE_AS
+# Ask Doubt on telegram @A_S_9162
 
 import time
 import math
