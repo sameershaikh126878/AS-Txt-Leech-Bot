bf57baa606b92a368d6390e968e8f9e77631f9eb
sameershaikh126878 <ywallah6@gmail.com>
2025-04-06 11:11:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -94,7 +94,7 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "█" * completed_length + "░" * remaining_length
             
             try:
-                await reply.edit(f'<b>\n ╭──⌯════𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚⬆️⬆️═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size -🕛 ETA » {tot} - {eta} \n├🤖 𝔹ʏ » @A_S_9162\n╰─═══ ✪ @SAMEER_OFFICAL_092 ✪ ═══─╯\n</b>') 
+                await reply.edit(f'<b>\n •╔════◄🌠𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🌠↑►════╗• \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size -🕛 ETA » {tot} - {eta} \n├🤖 𝐁𝐘 ➽ @A_S_9162\n•╚════◄@SAMEER_OFFICAL_092►════╝•\n</b>') 
             except FloodWait as e:
                 time.sleep(e.x)
 
